d5fdc8192507b8fcac3a7a19f5ac7d38620991b1
bindukasera89 <bindu89kasera@gmail.com>
2020-01-03 12:47:01 +0530

    change 1
    chnage 1

--- DTShopping/Models/order.cs
+++ DTShopping/Models/order.cs
@@ -88,6 +88,7 @@ namespace DTShopping.Models
         public Nullable<int> DispatchStatus { get; set; }
         public string PaymentMode { get; set; }
         public string PaymentStatus { get; set; }
+        public string PaymentDate { get; set; }        
     }
 
     public class PagedOrderList 
